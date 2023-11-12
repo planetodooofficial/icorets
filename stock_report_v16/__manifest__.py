c66a1760ed3771048d9a723f97d5839a9d3aadea
sawan sirswal <sawan.planetodoo@gmail.com>
2023-11-12 21:30:14 +0530

    Changes in reports

--- stock_report_v16/__manifest__.py
+++ stock_report_v16/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'PO Stock Reports v16',
+    'name': 'PO Stock Reports v15',
     'version': '16.0.0.1',
     'summary': 'Stock Reports',
     'sequence': 10,
     'description': """ """,
     'category': '',
     'website': 'https://www.planet-odoo.com',
-    'depends': ['stock'],
+    'depends': ["stock"],
     'data': [
         'security/ir.model.access.csv',
         'wizards/stock.xml',
