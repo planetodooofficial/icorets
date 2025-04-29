94b19ba25459f930e46c581505df9fb8b2507a6d
Akash Singh <akash@geotechnosoft.com>
2025-04-29 16:58:25 +0530

    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

--- gts_accounting/models/account_move.py
+++ gts_accounting/models/account_move.py
@@ -11,7 +11,7 @@ class AccountMove(models.Model):
     transporter_name = fields.Char(string="Transporter Name")
     lr_number = fields.Char(string="LR Number")
     logistic_number = fields.Char(string="L Number....")
-    customer_delivery_number = fields.Char(string="Customer Delivery Number")
+    customer_delivery_number = fields.Char(string="Delivery Status")
 
     quick_commerce = fields.Char(string="Quick Commerce")
     po_expiry_date = fields.Date(string="PO Expiry Date")
